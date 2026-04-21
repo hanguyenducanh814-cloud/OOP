f031063c32cdac74bb4f872edd0ea3a9584cd176
Nguyen Duc Anh <ducanh @gmail.com>
2026-04-21 11:21:55 +0700

    Hoàn thành bài 4

---
+++ src/MainFan.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MainFan {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSpeed(Fan.FAST);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
+
+        Fan fan2 = new Fan();
+        fan2.setSpeed(Fan.MEDIUM);
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        fan2.setOn(false);
+
+        System.out.println("Thông tin Fan 1: " + fan1.toString());
+        System.out.println("Thông tin Fan 2: " + fan2.toString());
+    }
+}
