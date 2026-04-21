6beacb965a462ea7e1f2d68330117e5aa770386a
Nguyen Duc Anh <ducanh @gmail.com>
2026-04-21 10:45:07 +0700

    Hoàn thành bài 3

---
+++ src/StopWatch.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
+
+    public static void main(String[] args) {
+        StopWatch sw = new StopWatch();
+        sw.stop();
+        System.out.println("Elapsed time: " + sw.getElapsedTime());
+    }
+}
